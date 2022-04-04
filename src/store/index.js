2662436764e572cf1ffb985fec49e6983e8c6ad6
Bogdan Shelomanov <bshelomanov@gmail.com>
2022-04-04 21:48:45 +0300

    lesson 7

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,34 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 import { profileReducer } from "./profile";
 import { conversationsReducer } from "./conversations";
 import { messagesReducer } from "./messages";
+import { logger, botMessage, timeScheduler } from "./middlewares";
 // import { createStore } from "./my-redux";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["messages"],
+  whitelist: ["profile"],
+};
+
+const reducer = combineReducers({
+  profile: profileReducer,
+  conversations: conversationsReducer,
+  messages: messagesReducer,
+});
+
 export const store = createStore(
-  combineReducers({
-    profile: profileReducer,
-    conversations: conversationsReducer,
-    messages: messagesReducer,
-  })
+  persistReducer(persistConfig, reducer),
+  compose(
+    applyMiddleware(thunk, logger, botMessage, timeScheduler),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (args) => args
+  )
 );
+
+export const persistor = persistStore(store);
